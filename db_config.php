c8e44654b96bca1ebcbaa9083023a4f55936420a
Joshua Odoi <odoijoshua55@gmail.com>
2020-02-07 09:56:35 +0000

    updated

--- db_config.php
+++ db_config.php
@@ -1,11 +1,11 @@
 <?php
 
 ob_start();
+session_start();
 
-
-$server_name = 'localhost';
-$user_name = 'root';
-$password = '';
-$database = 'barcode-soft';
+$server_name = '209.222.109.226';
+$user_name = 'liteneeds_admin';
+$password = 'orogneiL@55';
+$database = 'liteneeds_barcode_soft';
 
 $conn = mysqli_connect($server_name, $user_name, $password , $database);
